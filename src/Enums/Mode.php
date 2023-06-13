aa6359deacabea1c133ff8832561761cfed047d1
Jan Bladt <jan.bladt@syspons.com>
2023-06-13 16:13:57 +0200

    docs

--- src/Enums/Mode.php
+++ src/Enums/Mode.php
@@ -3,11 +3,12 @@
 namespace berthott\Targetable\Enums;
 
 /**
- * Mode for passing a class to {@see \berthott\Targetable\Services\TargetableService::__construct()}
+ * The mode TargetableService is operating in.
  * 
  * * **Trait** checks if the given trait is used by the class
  * * **Contract** checks if the given contract is implemented by the class
  * 
+ * @see \berthott\Targetable\Services\TargetableService::__construct()
  * @api
  */
 enum Mode
